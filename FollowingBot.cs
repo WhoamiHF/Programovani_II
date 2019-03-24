d89ac8d898fd1683d4b9b0b3fd04e0e323c5eae3
WhoamiHF <45389520+WhoamiHF@users.noreply.github.com>
2019-03-24 18:49:51 +0100

    Add files via upload

---
+++ FollowingBot.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Robocode;
+
+namespace Dave
+{
+    public class Class1
+    {
+        public class Dave : AdvancedRobot
+        {
+            public override void Run()
+            {
+               SetTurnRightRadians(double.PositiveInfinity);
+            }
+           
+            public override void OnScannedRobot(ScannedRobotEvent e)
+            {
+                double RadarTurn = Heading + e.Bearing -RadarHeading;
+                SetTurnRightRadians(RadarTurn);
+                SetAhead(50);
+            }
+        }
+    }
+}
