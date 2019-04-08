8c3a242701bc915e8ec23b36302d07b3ccc81247
WhoamiHF <45389520+WhoamiHF@users.noreply.github.com>
2019-04-08 13:07:01 +0200

    Add files via upload

---
+++ Fight.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Robocode;
+
+namespace Dave
+{
+    public class FollowingBot
+
+    {
+        public class Dave : AdvancedRobot
+        {
+            public override void Run()
+            {
+               SetTurnRightRadians(double.PositiveInfinity);
+               
+            }
+           
+            public override void OnScannedRobot(ScannedRobotEvent e)
+            {
+                
+
+                if (e.Distance <= 100)
+                {
+                    Fire(3);
+                }
+                else
+                {
+                    SetAhead(50);
+                    Fire(1);
+                }
+                double RadarTurn = e.Bearing;
+                SetTurnRightRadians(RadarTurn);
+            }
+        }
+    }
+}
